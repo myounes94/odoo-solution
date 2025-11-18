d7f45a9d2f1f2e1fa7e16915daae73e4308b0a03
myounes94 <m.younes.face@gmail.com>
2025-11-18 16:08:34 +0200

    Add files via upload

---
+++ admin-config.js
@@ -0,0 +1,5 @@
+// ملف الإعدادات - يمكن استبدال هذه القيم حسب الحاجة
+const ADMIN_CONFIG = {
+    WEB_APP_URL: "https://script.google.com/macros/s/AKfycbzuhz9LvI346WnMvjAgtBjISUolzj76Us0qryOa9MlBc2EL-L_uyn8FpLWObzggQBU/exec",
+    AUTH_KEY: "بسم الله توكلنا علي الله"
+};
